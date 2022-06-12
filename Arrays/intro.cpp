d17f6755403f76aff63cc59e21e0e700ec7bfe7d
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-06-12 09:46:11 -0700

    update:array intro

---
+++ Arrays/intro.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+// function for the priting the arrays
+void pritingarrays(int arr[],int n){
+    cout<<"priting the arrays"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+
+    }
+}
+
+int main(){
+    // declartion of an arrays
+    int arr[10];
+
+    // accessing an arrays element
+    // cout<<"value at 0 is "<<arr[0]<<endl;
+    // cout<<"value at 9 is"<<arr[9]<<endl;
+
+
+    // initalisation in the arrays 
+    int third[15]={2,7,3};
+    int n=15;
+    pritingarrays(third,15);
+
+
+
+
+}
