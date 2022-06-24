3e3f60ebd490259dc845856e4d0a65b32505182e
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-06-24 14:34:02 +0530

    update:uniqueElement
    in this question will find unique element in the arrays using xor operator which is optimise solution .

---
+++ uniqueElement.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int uniqueElements(int arr[],int n){
+
+    int ans=0;
+
+    // 
+    for(int i=0;i<n;i++){
+        ans=ans^arr[i];
+
+    }
+    return ans;
+
+}
+
+int main(){
+
+    
+}
